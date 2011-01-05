6601fa7ad963dd51b8b4b8e9faef34347405613e
xrzp.kkebb <xrzp.kkebb@8f5a9997-8157-8dfb-d7b8-2ef31ade41fa>
2011-01-05 06:38:30 +0000

    网速很不给力...

---
+++ trunk/src/com/frog4orcl/framework/BeanManager.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.frog4orcl.framework;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @˵�说明: 用于使用Spring提供的ApplicationContext类进行显式的Bean获取
+ * @author: dandan
+ * @email: xrzp_dh@yahoo.com.cn
+ * @create: Jan 5, 2011 2:29:35 PM
+ * @version: 1.0
+ */
+public class BeanManager {
+
+	private static ApplicationContext ac = null;
+
+	/**
+	 * 利用Spring实现声明式依赖注入，便于直接获取bean对象
+	 */
+	public static ApplicationContext getApplicationContext() {
+		return ac;
+	}
+
+	/**
+	 * 返回Spring的ApplicationContext对象
+	 * 
+	 * @return
+	 */
+	public static void setApplicationContext(ApplicationContext acObj) {
+		ac = acObj;
+	}
+
+	/**
+	 * 根据指定的bean名字来获取bean
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static Object getBean(String key) {
+		return ac.getBean(key);
+	}
+}
---
+++ trunk/src/com/frog4orcl/framework/Test1.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.frog4orcl.framework;
+
+/**
+ * @说明: 
+ * @author: dandan
+ * @email: xrzp_dh@yahoo.com.cn
+ * @create date:Jan 5, 2011 2:29:35 PM
+ * @version: 1.0
+ */
+public class Test1 {
+
+}
